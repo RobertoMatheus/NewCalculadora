35dfa8c2119664965815f2f5331103f0d3894cb5
RobertoMatheus <49045127+RobertoMatheus@users.noreply.github.com>
2022-05-14 22:36:28 -0300

    Add files via upload

---
+++ Calculadora/src/calculadora/Calc.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Pichau
+ */
+public interface Calc {
+    
+    String Calcular(int val1, int val2);
+}
---
+++ Calculadora/src/calculadora/Div.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Pichau
+ */
+public class Div implements  Calc   {
+   @Override
+   public String Calcular(int val1, int val2){
+               String txt = String.valueOf(val1/val2);
+               return txt;
+   }
+}
